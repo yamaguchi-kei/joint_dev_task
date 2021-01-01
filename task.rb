ee9b237786e2c85ec54a5e7dd7f728809559e831
yamaguchi-kei <rubylike1234@gmail.com>
2021-01-01 23:42:41 +0900

    ruby8 second

--- task.rb
+++ task.rb
@@ -75,7 +75,7 @@ def q8
 
   programming_languages.map(&:capitalize!)
 
-  upper_case_programming_languages = programming_languages.map(&:upcase!)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
 
   # 以下は変更しないで下さい
